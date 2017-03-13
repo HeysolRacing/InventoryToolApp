3fb72d4f3acdb427fb4975b15dae1555e1486c73
sugalde <sugalde@EFMMXCL-4452>
2017-03-13 13:16:40 -0600

    Suppliers changes and FeeCodes-Fix

--- InventoryTool/InventoryTool/Controllers/SsuppliersController.cs
+++ InventoryTool/InventoryTool/Controllers/SsuppliersController.cs
@@ -36,11 +36,11 @@ namespace InventoryTool.Controllers
             ViewBag.CurrentFilter = searchString;
 
             var suppliers = from s in db.Suppliers
-                        select s;
+                            select s;
             if (!String.IsNullOrEmpty(searchString))
             {
                 suppliers = suppliers.Where(s => s.SupplierName.ToString().Contains(searchString)
-                                       || s.Telephonefaxemail.ToString().Contains(searchString)
+                                       || s.Telephone.ToString().Contains(searchString)
                                        || s.ZIPCode.ToString().Contains(searchString));
             }
             switch (sortOrder)
@@ -49,7 +49,7 @@ namespace InventoryTool.Controllers
                     suppliers = suppliers.OrderByDescending(s => s.SupplierName);
                     break;
                 case "Telephone/fax/email":
-                    suppliers = suppliers.OrderBy(s => s.Telephonefaxemail);
+                    suppliers = suppliers.OrderBy(s => s.Telephone);
                     break;
                 case "ZIP Code":
                     suppliers = suppliers.OrderBy(s => s.ZIPCode);
@@ -62,7 +62,7 @@ namespace InventoryTool.Controllers
             int pageSize = 100;
             int pageNumber = (page ?? 1);
             return View(suppliers.ToPagedList(pageNumber, pageSize));
-            
+
         }
 
         public ActionResult Select(int? id, int cr, int screen)
@@ -86,6 +86,7 @@ namespace InventoryTool.Controllers
             { return RedirectToAction("Create", "CRs", new { id = cr }); }
             else { return RedirectToAction("Edit", "CRs", new { id = cr }); }
         }
+
         // GET: Ssuppliers/Details/5
         public ActionResult Details(int? id)
         {
@@ -112,7 +113,7 @@ namespace InventoryTool.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "SupplierID,SupplierName,Street,City,State,Country,ZIPCode,Affiliatestore,SupplierRate,Class,Franchise,Telephonefaxemail,ContactName,Status,Type,Rating,LastWAdate")] Ssupplier ssupplier)
+        public ActionResult Create([Bind(Include = "SupplierID,SupplierName,LegalName,Street,City,State,Country_cd,ZIPCode,StoreNumber,NatlAccountCode,BillMethod,DiscParts,DiscLabor,PaymentTerms,disc_cap_amt,supplier_typ_cd,Telephone,Fax,WebLink,email,ContactName,Status,Type,TaxID")] Ssupplier ssupplier)
         {
             if (ModelState.IsValid)
             {
@@ -144,7 +145,7 @@ namespace InventoryTool.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "SupplierID,SupplierName,Street,City,State,Country,ZIPCode,Affiliatestore,SupplierRate,Class,Franchise,Telephonefaxemail,ContactName,Status,Type,Rating,LastWAdate")] Ssupplier ssupplier)
+        public ActionResult Edit([Bind(Include = "SupplierID,SupplierName,LegalName,Street,City,State,Country_cd,ZIPCode,StoreNumber,NatlAccountCode,BillMethod,DiscParts,DiscLabor,PaymentTerms,disc_cap_amt,supplier_typ_cd,Telephone,Fax,WebLink,email,ContactName,Status,Type,TaxID")] Ssupplier ssupplier)
         {
             if (ModelState.IsValid)
             {
--- InventoryTool/InventoryTool/Models/Ssupplier.cs
+++ InventoryTool/InventoryTool/Models/Ssupplier.cs
@@ -8,33 +8,44 @@ namespace InventoryTool.Models
 {
     public class Ssupplier
     {
+        
         [Key]
         public int SupplierID { get; set; }
+        public int OracleID { get; set; }
+        public string SID { get; set; }
         [Display(Name = "Supplier Name")]
         public string SupplierName { get; set; }
+        [Display(Name = "Legal Name")]
+        public string LegalName { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string Country { get; set; }
+        public string Country_cd { get; set; }
         [Display(Name = "ZIP Code")]
         public int ZIPCode { get; set; }
         [Display(Name = "Affiliate Store")]
-        public string Affiliatestore { get; set; }
-        [Display(Name = "Supplier Rate")]
-        public decimal SupplierRate { get; set; }
-        public string Class { get; set; }
-        public string Franchise { get; set; }
-        [Display(Name = "Telephone/fax/email")]
-        public string Telephonefaxemail { get; set; }
+        public string StoreNumber { get; set; }
+        [Display(Name = "Account Code")]
+        public int NatlAccountCode { get; set; }
+        [Display(Name = "Bill Method")]
+        public string BillMethod { get; set; }
+        public int DiscParts { get; set; }
+        public int DiscLabor { get; set; }
+        public int PaymentTerms { get; set; }
+        public int disc_cap_amt { get; set; }
+        public string supplier_typ_cd { get; set; }
+        [Display(Name = "Main phone")]
+        public string Telephone{ get; set; }
+        [Display(Name = "Fax")]
+        public string Fax { get; set; }
+        [Display(Name = "Web Link")]
+        public string WebLink { get; set; }
+        [Display(Name = "E-mail")]
+        public string email { get; set; }
         [Display(Name = "Contact Name")]
         public string ContactName  { get; set; }
         public string Status { get; set; }
         public string Type { get; set; }
-        public decimal Rating { get; set; }
-        [Display(Name = "Last WA Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
-        public DateTime LastWAdate { get; set; }
-
+        public string TaxID { get; set; }
     }
 }
